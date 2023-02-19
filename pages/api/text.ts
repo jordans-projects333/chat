b67177da5d2a895933cfb2f3f41e894255189046
Jordan <jordanroberts333@icloud.com>
2023-02-19 23:44:51 +0000

    facebook auth

--- pages/api/text.ts
+++ pages/api/text.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import twilio from 'twilio';
 export default function sendMessage(req: NextApiRequest, res: NextApiResponse) {
-  const accountSid = <string>process.env.TWILIO_ACCOUNT_SID;
-  const token = <string>process.env.TWILIO_AUTH_TOKEN;
+  const accountSid = <string>process.env.ACCOUNT_SID;
+  const token = <string>process.env.AUTH_TOKEN;
+  console.log((process.env.ACCOUNT_SID))
   const client = twilio(accountSid, token);
   // const { phone, message } = req.body;
   // console.log(phone, message);
   client.messages
     .create({
-      body: 'cuddle time',
+      body: 'https://chat-jordans-projects333.vercel.app , youve been sent a message',
       from: '+16075645663',
       to: '+447720861605',
     })
